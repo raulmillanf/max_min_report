99b32f891639b4151304ff5858dadea0bcc189f2
Raul Millan <Raulmillanfle@gmail.com>
2020-07-14 22:18:49 -0400

    Cambio de posición de campos Costo y Cantidad a mano/Corrección de traducción de qty_available

--- models/product_id_cost.py
+++ models/product_id_cost.py
@@ -8,4 +8,4 @@ class MaxMinReport(models.Model):
     _inherit = 'stock.warehouse.orderpoint'
     #_name = 'max_min_report'
     product_cost = fields.Float('Costo', digits=dp.get_precision('Product Price'),related="product_id.product_cost",  groups="purchase.group_purchase_manager" ,help = "Costo referencial del producto.")
-    qty_available = fields.Float('Quantity On Hand', related="product_id.qty_available", search='_search_qty_available', digits=dp.get_precision('Product Unit of Measure'))
+    qty_available = fields.Float('Cantidad a mano', related="product_id.qty_available", search='_search_qty_available', digits=dp.get_precision('Product Unit of Measure'))

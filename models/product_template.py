f35d2321003b009fb8701dd09c40222101050899
Raul Millan <Raulmillanfle@gmail.com>
2020-07-22 03:35:36 -0400

    Corrección de código py, Traducción de nombre de cost_currency_id. se agrego este campo al modelo product.product

--- models/product_template.py
+++ models/product_template.py
@@ -13,7 +13,7 @@ class ProductTemplateCost(models.Model):
         return self.env.user.company_id.currency_id
 
 
-    cost_currency_id = fields.Many2one('res.currency', string="Cost List Currency", default=_default_Ccurrency)
+    cost_currency_id = fields.Many2one('res.currency', string="Moneda para costo", groups="purchase.group_purchase_manager", default=_default_Ccurrency)
 
     @api.multi
     def cost_compute(self, price_type, uom=False, currency=False, company=False):
@@ -29,3 +29,25 @@ class ProductTemplateCost(models.Model):
                 cost[template.id] = template.cost_currency_id.compute(cost[template.id],
                                                                          currency or template.currency_id, round=False)
         return cost
+
+        
+
+class ProductProductcost(models.Model):
+    _inherit = 'product.product'
+
+
+    @api.multi
+    def cost_compute(self, price_type, uom=False, currency=False, company=False):
+
+        if not currency and self._context.get('currency'):
+            currency = self.env['res.currency'].browse(self._context['currency'])
+
+        templates = self.with_context(currency=False)
+        cost = super(ProductTemplateCost, templates).cost_compute(price_type, uom, currency=False,  company=company)
+        if price_type in ['product_cost', 'product_cost']:  
+            for template in self:
+
+                cost[template.id] = template.cost_currency_id.compute(cost[template.id],
+                                                                         currency or template.currency_id, round=False)
+        return cost
+

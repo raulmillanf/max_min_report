036aa09bd20c200f90c04587ebeaaa8e2f40e3e9
Raul Millan <Raulmillanfle@gmail.com>
2020-07-29 02:01:35 -0400

    Se agregaron la linea comercial y la agrupación por marca y linea comercial

--- models/product_id_cost.py
+++ models/product_id_cost.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#Created by Ramf
 
 import odoo.addons.decimal_precision as dp
 from odoo import fields, models, api
@@ -10,5 +11,13 @@ class MaxMinReport(models.Model):
 
     product_cost = fields.Float('Costo', digits=dp.get_precision('Product Price'), related="product_id.product_cost",  groups="purchase.group_purchase_manager" ,help = "Costo referencial del producto.")
     qty_available = fields.Float('Cantidad a mano', related="product_id.qty_available", search='_search_qty_available', digits=dp.get_precision('Product Unit of Measure'))
-    product_brand_id = fields.Char('Marca', related='product_id.product_brand_id.name')
-    cost_currency = fields.Char(string='Moneda', related='product_id.cost_currency.symbol')
+    product_brand_id = fields.Char('Marca', related='product_id.product_brand_id.name', store=True)
+    product_commer_id = fields.Char('Linea comercial', related='product_id.product_commer_id.name', store=True)
+    cost_currency = fields.Char(' ', related='product_id.cost_currency.symbol')
+    sellers_str = fields.Char('Proveedor', compute = "_get_seller_name", store=True)
+
+    def _get_seller_name(self):
+        for record in self:
+            ids= record.product_id.seller_ids.name
+            if ids:
+                record.sellers_str = record.product_id.seller_ids.name[0]
